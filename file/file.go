c420300974d8433325d0bd0b3f75f65e7dd7fbf7
UlricQin <ulric.qin@gmail.com>
2014-03-21 15:41:51 +0800

    add file utils

---
+++ file/file.go
@@ -0,0 +1,137 @@
+package file
+
+import (
+	"path"
+	"path/filepath"
+	"os"
+	"errors"
+	"io/ioutil"
+)
+
+// SelfPath gets compiled executable file absolute path
+func SelfPath() string {
+	path, _ := filepath.Abs(os.Args[0])
+	return path
+}
+
+// SelfDir gets compiled executable file directory
+func SelfDir() string {
+	return filepath.Dir(SelfPath())
+}
+
+// get filepath base name
+func Basename(file string) string {
+	return path.Base(file)
+}
+
+// get filepath dir name
+func Dir(file string) string {
+	return path.Dir(file)
+}
+
+// rename file name
+func Rename(file string, to string) error {
+	return os.Rename(file, to)
+}
+
+// delete file
+func Unlink(file string) error {
+	return os.Remove(file)
+}
+
+// rename file name
+func Rename(file string, to string) error {
+	return os.Rename(file, to)
+}
+
+// IsFile checks whether the path is a file,
+// it returns false when it's a directory or does not exist.
+func IsFile(filePath string) bool {
+	f, e := os.Stat(filePath)
+	if e != nil {
+		return false
+	}
+	return !f.IsDir()
+}
+
+// IsExist checks whether a file or directory exists.
+// It returns false when the file or directory does not exist.
+func IsExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil || os.IsExist(err)
+}
+
+// Search a file in paths.
+// this is often used in search config file in /etc ~/
+func SearchFile(filename string, paths ...string) (fullPath string, err error) {
+	for _, path := range paths {
+		if fullPath = filepath.Join(path, filename); IsExist(fullPath) {
+			return
+		}
+	}
+	err = errors.New(fullPath + " not found in paths")
+	return
+}
+
+// get absolute filepath, based on built executable file
+func RealPath(file string) (string, error) {
+	if path.IsAbs(file) {
+		return file, nil
+	}
+	wd, err := os.Getwd()
+	return path.Join(wd, file), err
+}
+
+// get file modified time
+func FileMTime(file string) (int64, error) {
+	f, e := os.Stat(file)
+	if e != nil {
+		return 0, e
+	}
+	return f.ModTime().Unix(), nil
+}
+
+// get file size as how many bytes
+func FileSize(file string) (int64, error) {
+	f, e := os.Stat(file)
+	if e != nil {
+		return 0, e
+	}
+	return f.Size(), nil
+}
+
+// SaveFile saves content type '[]byte' to file by given path.
+// It returns error when fail to finish operation.
+func WriteBytesToFile(filePath string, b []byte) (int, error) {
+	os.MkdirAll(path.Dir(filePath), os.ModePerm)
+	fw, err := os.Create(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer fw.Close()
+	return fw.Write(b)
+}
+
+// SaveFileS saves content type 'string' to file by given path.
+// It returns error when fail to finish operation.
+func WriteStringFile(filePath string, s string) (int, error) {
+	return WriteBytesToFile(filePath, []byte(s))
+}
+
+// ReadFile reads data type '[]byte' from file by given path.
+// It returns error when fail to finish operation.
+func ReadFileToBytes(filePath string) ([]byte, error) {
+	b, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return []byte(""), err
+	}
+	return b, nil
+}
+
+// ReadFileS reads data type 'string' from file by given path.
+// It returns error when fail to finish operation.
+func ReadFileToString(filePath string) (string, error) {
+	b, err := ReadFileToBytes(filePath)
+	return string(b), err
+}
+
