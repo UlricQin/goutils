13c3a546d433213d8796083d8fc8c20ab3c0bb24
Ulric Qin <cnperl@163.com>
2014-03-27 21:07:12 +0800

    add formatter and paginator

--- convertor/convertor.go
+++ convertor/convertor.go
@@ -2,6 +2,8 @@ package convertor
 
 import (
 	"encoding/binary"
+	"reflect"
+	"fmt"
 )
 
 func Int64ToBytes(i int64) []byte {
@@ -14,4 +16,16 @@ func BytesToInt64(buf []byte) int64 {
 	return int64(binary.BigEndian.Uint64(buf))
 }
 
-
+// convert any numeric value to int64
+func ToInt64(value interface{}) (d int64, err error) {
+	val := reflect.ValueOf(value)
+	switch value.(type) {
+	case int, int8, int16, int32, int64:
+		d = val.Int()
+	case uint, uint8, uint16, uint32, uint64:
+		d = int64(val.Uint())
+	default:
+		err = fmt.Errorf("ToInt64 need numeric not `%T`", value)
+	}
+	return
+}
